7de6b671f0ff8c636812976686e32626e060f990
Pasqat <pasquale.matarrese@gmail.com>
2020-06-23 18:59:19 +0200

    Add userId to streams, create hystory object with createHistory and change BrowserRouter with Router

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "./apis/streams";
+import history from "../history";
 
 import {
   SIGN_IN,
@@ -17,16 +18,19 @@ export const signIn = (userId) => {
   };
 };
 
+
 export const signOut = () => {
   return {
     type: SIGN_OUT,
   };
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
+import history from '../history';
 
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
@@ -11,14 +12,14 @@ import Header from "./Header";
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Route path="/" exact component={StreamList} />
         <Route path="/streams/new" exact component={StreamCreate} />
         <Route path="/streams/edit" exact component={StreamEdit} />
         <Route path="/streams/delete" exact component={StreamDelete} />
         <Route path="/streams/show" exact component={StreamShow} />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
